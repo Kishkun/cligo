358234971ed2333a23ba2bfb0fc99d3bef43666e
Kishkun <durata55@gmail.com>
2019-01-25 21:01:42 +0300

    add mobile version and the end project

--- js/common.js
+++ js/common.js
@@ -29,3 +29,19 @@ if(mainButtonEl) {
     mainButtonEl.addEventListener('click', onOpenPopupWrapperClick);
     closeButtonEl.addEventListener('click', onClosePopupWrapperClick);
 }
+// header-mobile-button
+let mobileButton = document.querySelector('.header-mobile-button-body');
+let mobileButtonLine = document.querySelectorAll('.header-mobile-button-line');
+let headerMobile = document.querySelector('.header-mobile');
+let headerScroll = document.querySelector('body');
+
+let clickMobileButton = function () {
+    for (let i = 0; i < mobileButtonLine.length; i++) {
+        mobileButtonLine[i].classList.toggle('header-mobile-button-line-active')
+    }
+    headerMobile.classList.toggle('header-mobile-active');
+    headerScroll.classList.toggle('header_scroll_out');
+};
+if (mobileButton) {
+    mobileButton.addEventListener('click', clickMobileButton);
+}
